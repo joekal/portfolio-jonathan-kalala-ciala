075563491801854b15e85a56ccc3fbd932a3e62f
jonathan <kalalajonathan297@gmail.com>
2026-08-11 09:23:52 +0100

    lancement du project 1

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Route pour la racine de l'app
+    path('certifications/', views.certifications, name='certifications'),
+    path('projets/', views.projets, name='projets'),
+    path('articles/', views.articles, name='articles'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, 'app/index.html')
+
+
+def certifications(request):
+    return render(request, 'app/certifications.html')
+
+
+def projets(request):
+    return render(request, 'app/projects.html')
+
+
+def articles(request):
+    return render(request, 'app/articles.html')
