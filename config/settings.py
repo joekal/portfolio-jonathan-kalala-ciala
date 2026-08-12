4210b8174412868663c9cef4af1a74623e819956
jonathan <kalalajonathan297@gmail.com>
2026-08-12 09:55:37 +0100

    lancement du projet 3

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'app' / 'static']
+STATICFILES_DIRS = []
+for static_dir in (BASE_DIR / 'app' / 'static', BASE_DIR / 'static'):
+    if static_dir.exists():
+        STATICFILES_DIRS.append(static_dir)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
